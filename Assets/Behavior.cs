f534c4b045fd55232008634a7e91683862da26d3
Daniel <daniel_jr_nichols@hotmail.com>
2017-02-09 14:33:13 -0500

    Ajout des controls du joueur

---
+++ Assets/Behavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior : MonoBehaviour {
+    [SerializeField]
+    float speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("d"))
+            transform.position += new Vector3(1f * speed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey("a"))
+            transform.position += new Vector3(-1f * speed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey("s"))
+            transform.position += new Vector3(0f, -1f * speed * Time.deltaTime, 0f);
+        if (Input.GetKey("w"))
+            transform.position += new Vector3(0f, 1f * speed * Time.deltaTime, 0f);
+    }
+}
