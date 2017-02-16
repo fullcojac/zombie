974183de92e8651de5e1ed0aacbe76a157402082
Daniel <daniel_jr_nichols@hotmail.com>
2017-02-15 21:57:29 -0500

    new sprite

---
+++ Assets/Script/BoardManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+    public GameObject wall;
+    public GameObject door;
+    Vector3 randomDoorPosition;
+    // Use this for initialization
+    void Start ()
+    {
+        randomDoorPosition = wall.transform.GetChild(Random.Range(0, wall.transform.childCount)).position;
+        Instantiate(door, randomDoorPosition, Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
